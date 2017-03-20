cc380051fcedf8fccd710d549473a5f02e27147f
FeL1ksS <fel1kssx@gmail.com>
2017-03-20 23:09:38 +0300

    AccountJS

--- js/login.js
+++ js/login.js
@@ -1,10 +1,28 @@
 Vue.use(VueMaterial);
 var Account = new Vue({
-   el: "#Account",
+    el: "#Account",
     data: {
-        welcom: true,
-        register: false,
-        logim: false
+        window: {
+            welcom: true,
+            register: false,
+            logim: false
+        },
+        dataLogin: {
+            telephone: '',
+            password: ''
+        },
+        dataRegister: {
+            name: '',
+            lastname: '',
+            telephone: '',
+            password: ''
+        }
+    },
+    methods: {
+        log: function (){
+            console.log(this.dataLogin);
+            console.log(this.dataRegister);
+        }                  
     }
 
 });
